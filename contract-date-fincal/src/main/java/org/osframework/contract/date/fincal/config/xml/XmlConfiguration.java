29b0aa311daa433377ce6f4ddd03f0cae875149b
Dave Joyce <david.joyce13@gmail.com>
2013-01-10 08:25:30 -0500

    Added commons-io dependency; work on XmlConfiguration

--- contract-date-fincal/src/main/java/org/osframework/contract/date/fincal/config/xml/XmlConfiguration.java
+++ contract-date-fincal/src/main/java/org/osframework/contract/date/fincal/config/xml/XmlConfiguration.java
@@ -17,14 +17,27 @@
  */
 package org.osframework.contract.date.fincal.config.xml;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
+import org.apache.commons.io.IOUtils;
 import org.osframework.contract.date.fincal.config.AbstractConcurrentMapConfiguration;
 import org.osframework.contract.date.fincal.config.ConfigurationException;
+import org.slf4j.Logger;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
 /**
  * XmlConfiguration description here.
@@ -39,6 +52,8 @@ public class XmlConfiguration extends AbstractConcurrentMapConfiguration {
 			load(new FileInputStream(xmlFile));
 		} catch (IOException ioe) {
 			throw new ConfigurationException("Cannot load Configuration from XML", ioe);
+		} catch (SAXException se) {
+			throw new ConfigurationException("Cannot parse XML", se);
 		}
 		this.observer.configurationCreated(this);
 	}
@@ -53,6 +68,8 @@ public class XmlConfiguration extends AbstractConcurrentMapConfiguration {
 			load(xmlIn);
 		} catch (IOException ioe) {
 			throw new ConfigurationException("Cannot load Configuration from XML", ioe);
+		} catch (SAXException se) {
+			throw new ConfigurationException("Cannot parse XML", se);
 		}
 		this.observer.configurationCreated(this);
 	}
@@ -63,12 +80,57 @@ public class XmlConfiguration extends AbstractConcurrentMapConfiguration {
 			load(xmlUrl.openStream());
 		} catch (IOException ioe) {
 			throw new ConfigurationException("Cannot load Configuration from XML", ioe);
+		} catch (SAXException se) {
+			throw new ConfigurationException("Cannot parse XML", se);
 		}
 		this.observer.configurationCreated(this);
 	}
 
-	private void load(final InputStream xmlIn) throws IOException {
-		// TODO Write this.
+	private void load(final InputStream xmlIn) throws IOException, SAXException {
+		// Copy and close the given input stream
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		IOUtils.copy(xmlIn, baos);
+		IOUtils.closeQuietly(xmlIn);
+		
+		// Create 2 copies of XML input: 1 for validation, 1 for parse
+		InputStream validatorIn = new ByteArrayInputStream(baos.toByteArray()),
+				    parserIn    = new ByteArrayInputStream(baos.toByteArray());
+		
+		// Validate configuration XML
+		SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+		Schema xsd = sf.newSchema(this.getClass().getResource(XmlConstants.XSD_FILENAME));
+		Validator validator = xsd.newValidator();
+		validator.setErrorHandler(new ValidationErrorHandler(this.logger));
+		validator.validate(new StreamSource(validatorIn));
+		
+		// Parse configuration XML
 	}
 
+	private class ValidationErrorHandler implements ErrorHandler {
+	
+		private final Logger logger;
+	
+		private ValidationErrorHandler(Logger logger) {
+			this.logger = logger;
+		}
+	
+		@Override
+		public void warning(SAXParseException exception) throws SAXException {
+			logger.warn("Validation Warning", exception);
+			throw exception;
+		}
+
+		@Override
+		public void error(SAXParseException exception) throws SAXException {
+			logger.error("Validation Error", exception);
+			throw exception;
+		}
+
+		@Override
+		public void fatalError(SAXParseException exception) throws SAXException {
+			logger.error("Validation Fatal Error", exception);
+			throw exception;
+		}
+		
+	}
 }
