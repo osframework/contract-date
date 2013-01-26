fd9f3147fdffc72da2e076e277b898e1abca3284
Dave Joyce <david.joyce13@gmail.com>
2013-01-26 14:28:39 -0500

    Finished FinancialCalendarGeneratorBuilderTest

--- contract-date-fincal/src/test/java/org/osframework/contract/date/fincal/FinancialCalendarGeneratorBuilderTest.java
+++ contract-date-fincal/src/test/java/org/osframework/contract/date/fincal/FinancialCalendarGeneratorBuilderTest.java
@@ -22,9 +22,16 @@ import static org.testng.Assert.*;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
 
+import org.apache.commons.lang.math.IntRange;
 import org.osframework.contract.date.fincal.config.Configuration;
 import org.osframework.contract.date.fincal.config.xml.DefaultXmlConfiguration;
+import org.osframework.contract.date.fincal.model.FinancialCalendar;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
@@ -49,10 +56,54 @@ public class FinancialCalendarGeneratorBuilderTest {
 	@Test(groups="generator",
 		  dependsOnGroups={"config","data","producer"},
 		  expectedExceptions=IllegalArgumentException.class)
-	public void testAddCalendarInvalidCalendar() {
+	public void testAddInvalidCalendar() {
 		FinancialCalendarGeneratorBuilder builder = new FinancialCalendarGeneratorBuilder(config);
 		builder.addCalendar("XYZ");
 		fail("Expected IllegalArgumentException to be thrown");
+	}
+
+	@Test(groups="generator",
+		  dependsOnGroups={"config","data","producer"},
+		  dataProvider="calendarIds")
+	public void testAddCalendar(String calendarId) {
+		FinancialCalendarGeneratorBuilder builder = new FinancialCalendarGeneratorBuilder(config);
+		builder.addCalendar(calendarId);
+	}
+
+	@Test(groups="generator",
+		  dependsOnGroups={"config","data","producer"},
+		  dataProvider="invalidYears",
+		  expectedExceptions=IllegalArgumentException.class)
+	public void testSetInvalidFirstYear(int year) {
+		FinancialCalendarGeneratorBuilder builder = new FinancialCalendarGeneratorBuilder(config);
+		builder.setFirstYear(year);
+		fail("Expected IllegalArgumentException to be thrown");
+	}
+
+	@Test(groups="generator",
+		  dependsOnGroups={"config","data","producer"},
+		  dataProvider="validYears")
+	public void testSetFirstYear(int year) {
+		FinancialCalendarGeneratorBuilder builder = new FinancialCalendarGeneratorBuilder(config);
+		builder.setFirstYear(year);
+	}
+
+	@Test(groups="generator",
+		  dependsOnGroups={"config","data","producer"},
+		  dataProvider="invalidYears",
+		  expectedExceptions=IllegalArgumentException.class)
+	public void testSetInvalidLastYear(int year) {
+		FinancialCalendarGeneratorBuilder builder = new FinancialCalendarGeneratorBuilder(config);
+		builder.setLastYear(year);
+		fail("Expected IllegalArgumentException to be thrown");
+	}
+
+	@Test(groups="generator",
+		  dependsOnGroups={"config","data","producer"},
+		  dataProvider="validYears")
+	public void testSetLastYear(int year) {
+		FinancialCalendarGeneratorBuilder builder = new FinancialCalendarGeneratorBuilder(config);
+		builder.setLastYear(year);
 	}
 
 	@Test(groups="generator",
@@ -64,6 +115,24 @@ public class FinancialCalendarGeneratorBuilderTest {
 		fail("Expected FinancialCalendarException to be thrown");
 	}
 
+	@Test(groups="generator",
+		  dependsOnGroups={"config","data","producer"},
+		  dataProvider="builderConfigParams")
+	public void testBuild(String[] calendarIds,
+			              int firstYear,
+			              int lastYear,
+			              boolean weekends,
+			              Class<?> expectedType) throws FinancialCalendarException {
+		FinancialCalendarGeneratorBuilder builder = new FinancialCalendarGeneratorBuilder(config);
+		// Add all calendars
+		for (String calendarId : calendarIds) {
+			builder.addCalendar(calendarId);
+		}
+		builder.setFirstYear(firstYear).setLastYear(lastYear).includeWeekends(weekends);
+		FinancialCalendarGenerator generator = builder.build();
+		assertEquals(generator.getClass(), expectedType);
+	}
+
 	@BeforeClass
 	public void beforeClass() throws IOException {
 		URL configXml = loadDefinitionsXML();
@@ -73,6 +142,69 @@ public class FinancialCalendarGeneratorBuilderTest {
 	@AfterClass
 	public void afterClass() {
 		this.config = null;
+	}
+
+	@DataProvider
+	public Object[][] calendarIds() {
+		Iterator<FinancialCalendar> it = config.financialCalendarIterator();
+		List<String> calendarIds = new ArrayList<String>();
+		while (it.hasNext()) {
+			calendarIds.add(it.next().getId());
+		}
+		Object[][] results = new Object[calendarIds.size()][];
+		for (int i = 0; i < calendarIds.size(); i++) {
+			results[i] = new Object[] { calendarIds.get(i) };
+		}
+		return results;
+	}
+
+	@DataProvider
+	public Object[][] invalidYears() {
+		return new Object[][] {
+			new Object[] { 1899 },
+			new Object[] { 5000 }
+		};
+	}
+
+	@DataProvider
+	public Iterator<Object[]> validYears() {
+		final IntRange years = new IntRange(1900, 4099);
+		return new Iterator<Object[]>() {
+			int next = years.getMinimumInteger();
+			public boolean hasNext() {
+				return (years.containsInteger(next));
+			}
+			public Object[] next() {
+				if (!hasNext()) {
+					throw new NoSuchElementException();
+				}
+				return new Object[] { next++ };
+			}
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+		};
+	}
+
+	@DataProvider
+	public Object[][] builderConfigParams() {
+		Object[] set1 = new Object[] {
+			new String[] { "NYB", "LnB" },
+			2012,
+			2061,
+			true,
+			ConcurrentFinancialCalendarGenerator.class
+		};
+		Object[] set2 = new Object[] {
+			new String[] { "NYB", "LnB" },
+			2012,
+			2021,
+			false,
+			SequentialFinancialCalendarGenerator.class
+		};
+		return new Object[][] {
+			set1, set2
+		};
 	}
 
 }
